45292c3b71a121e76ad8f3545dbcac24df45cfa0
Maxence <maxence.levelu@outlook.fr>
2021-10-21 18:06:49 +0200

    First commit

---
+++ api/src/test/kotlin/com/blogapi/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.blogapi.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
